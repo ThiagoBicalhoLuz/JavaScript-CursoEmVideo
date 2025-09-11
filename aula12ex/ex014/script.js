93f374aa3d6837b415f4ac50fcf45db49af60461
Thiago Luz <thiagobicalholuz@gmail.com>
2025-09-10 21:05:24 -0300

    Exercicios novos ex014

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar () {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#139956ff'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#84582bff'
+    } else {
+        //BOA NOITE!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#2c3a3f'
+    }
+}
