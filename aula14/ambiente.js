a2780d101c6efbd942fa92b5748f8603db8b35e7
Thiago Luz <thiagobicalholuz@gmail.com>
2025-09-30 20:19:01 -0300

    Conclusão Repetições e Debug/Depuração

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -3,8 +3,14 @@ while (c <= 5) {
     console.log(c)
     c++
 }
-*/
 
-for(var c = 1; c <= 13; c++) {
+
+for(var c = 1; c <= 5; c++) {
     console.log(c)
 }
+*/
+console.log('Vai começar...')
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('Fim!')
